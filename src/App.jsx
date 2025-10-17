c27bbe931bc091d5fc47b2e9e9ffbd89ded7fcfb
Thirumalesu <thirumalesuguturu5@gmail.com>
2025-10-17 20:00:50 +0530

    17th oct Tasklist pt

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Taskform from "./Components/Taskform.jsx"
 import TaskList from "./Components/TaskList.jsx"
 import Progresstracker from "./Components/Progresstracker.jsx"
 import { useEffect, useState } from "react";
+import "./style.css"
 export default function App() {
   
   const [tasks, setTasks] = useState([]);
@@ -13,13 +14,23 @@ export default function App() {
     setTasks([...tasks,task]);
   }
 
+  const updateTask =(updatedTask, index) => {
+    const newtask = [...tasks];
+    newtask[index] = updatedTask;
+    setTasks(newtask); 
+  }
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((_, i) => i != index));
+  }
   return (
     <div>
       <h1>TASK FOCUS</h1>
       <p>Our Friendly TaskManager</p>
       <Taskform addTask = {addTask}/>
-          <TaskList />
-          <Progresstracker />
+          <TaskList tasks = {tasks} 
+          updateTask = {updateTask} deleteTask = {deleteTask}/>
+          
+          <Progresstracker tasks ={tasks}/>
           <button>Clear All tasks</button>
     </div>
   )
